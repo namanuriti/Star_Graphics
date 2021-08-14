f313291ebe0c0b1d70b77e10cafd58223a6419e5
Naman Uriti <namanuriti@gmail.com>
2021-08-14 12:56:11 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#Key of * is Shift + 8
+Stars = {1:'* * *', 2:'*   *', 3:'*    ', 4:' * * *'}
+A = f'{Stars[1]}\n{Stars[2]}\n{Stars[1]}\n{Stars[2]}\n{Stars[2]}'
+B = f'{Stars[1]}\n{Stars[2]}\n{Stars[1]}\n{Stars[2]}\n{Stars[1]}'
+C = f'{Stars[4]}\n{Stars[3]}\n{Stars[3]}\n{Stars[3]}\n{Stars[4]}'
+D = f'{Stars[1]}\n{Stars[5]}\n{Stars[5]}\n{Stars[5]}\n{Stars[1]}'
+E = f'{Stars[1]}\n{Stars[3]}\n{Stars[1]}\n{Stars[3]}\n{Stars[1]}'
+F = f'{Stars[1]}\n{Stars[3]}\n{Stars[1]}\n{Stars[3]}\n{Stars[3]}'
+BED = f'{B}\n\n\n{E}\n\n\n{D}'
+FED = f'{F}\n\n\n{E}\n\n\n{D}'
+BEAF = f'{B}\n\n\n{E}\n\n\n{A}\n\n\n{F}'
+BEAF = f'{F}\n\n\n{E}\n\n\n{E}\n\n\n{D}'
